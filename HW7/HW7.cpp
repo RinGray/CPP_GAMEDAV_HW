02be613b1004806dd7e19d72fa0ddd2cb277ee56
RinRay <igorproskurenko@gmail.com>
2025-01-21 22:40:17 +0200

    fix and delate excess code

--- HW7/HW7.cpp
+++ HW7/HW7.cpp
@@ -7,45 +7,42 @@
 
 using namespace std;
 
-enum class gameVariable
+enum class Token
 {
     Rock = 'r',
     Paper = 'p',
     Scissor = 's',
-    Zero = 0
 };
 
-int calculateResult(gameVariable enteredKey, gameVariable two, gameVariable three, gameVariable four) {
-    //std::srand(std::time(nullptr));
-    if (((enteredKey == two) and (enteredKey == three)) and (enteredKey == four)) {
-        if (enteredKey == gameVariable::Rock) {
-            std::cout << "Computer choice Paper" << std::endl;
-        }
-        if (enteredKey == gameVariable::Paper) {
-            std::cout << "Computer choice Scissor" << std::endl;
-        }
-        if (enteredKey == gameVariable::Scissor) {
-            std::cout << "Computer choice Rock" << std::endl;
-        }
+int calculateResult(Token enteredKey) {
+    std::srand((unsigned int)time(NULL));
+    const int GeneratingRange = 3;
+    const double GeneratedNum = std::rand() % GeneratingRange + 1;
 #ifdef _DEBUG
-        std::cout << "!!!!!!!!!!!!!!!!!!!!! STREAK !!!!!!!!!!!!!!!!!!!!!" << std::endl;
+    std::cout << "*** RANDOM NUMBER *** // " << GeneratedNum << " // *** RANDOM NUMBER ***" << std::endl;
 #endif // DEBUG
-        return 0;
+    if (GeneratedNum == 1) {
+        std::cout << "Computer choice Rock" << std::endl;
+        if (enteredKey == Token::Rock) {
+            return 99;
+        }
+        else {
+            if (enteredKey == Token::Paper) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
     }
     else {
-        std::srand((unsigned int)time(NULL));
-        const int GeneratingRange = 3;
-        const double GeneratedNum = std::rand() % GeneratingRange + 1;
-#ifdef _DEBUG
-        std::cout << "*** RANDOM NUMBER *** // " << GeneratedNum << " // *** RANDOM NUMBER ***" << std::endl;
-#endif // DEBUG
-        if (GeneratedNum == 1) {
-            std::cout << "Computer choice Rock" << std::endl;
-            if (enteredKey == gameVariable::Rock) {
+        if (GeneratedNum == 2) {
+            std::cout << "Computer choice Paper" << std::endl;
+            if (enteredKey == Token::Paper) {
                 return 99;
             }
             else {
-                if (enteredKey == gameVariable::Paper) {
+                if (enteredKey == Token::Scissor) {
                     return 1;
                 }
                 else {
@@ -54,37 +51,22 @@ int calculateResult(gameVariable enteredKey, gameVariable two, gameVariable thre
             }
         }
         else {
-            if (GeneratedNum == 2) {
-                std::cout << "Computer choice Paper" << std::endl;
-                if (enteredKey == gameVariable::Paper) {
-                    return 99;
-                }
-                else {
-                    if (enteredKey == gameVariable::Scissor) {
-                        return 1;
-                    }
-                    else {
-                        return 0;
-                    }
-                }
+            std::cout << "Computer choice Scissor" << std::endl;
+            if (enteredKey == Token::Scissor) {
+                return 99;
             }
             else {
-                std::cout << "Computer choice Scissor" << std::endl;
-                if (enteredKey == gameVariable::Scissor) {
-                    return 99;
+                if (enteredKey == Token::Rock) {
+                    return 1;
                 }
                 else {
-                    if (enteredKey == gameVariable::Rock) {
-                        return 1;
-                    }
-                    else {
-                        return 0;
-                    }
+                    return 0;
                 }
             }
         }
     }
 }
+
 
 int message(int r) {
     if (r == 0) {
@@ -104,10 +86,7 @@ int message(int r) {
 
 int main()
 {
-    gameVariable two = gameVariable::Zero;
-    gameVariable three = gameVariable::Zero;
-    gameVariable four = gameVariable::Zero;
-    int r=6, wins, losses, draws;
+    int r, wins, losses, draws;
     char rounds, k;
     std::cout << "Welcom to game" << std::endl;
     while (true) {
@@ -135,16 +114,16 @@ int main()
                     k = getchar();
                     cin.get();
                     std::cout << std::endl;
-                    gameVariable key = static_cast<gameVariable>(k);
+                    Token key = static_cast<Token>(k);
                     switch (key)
                     {
-                    case (gameVariable::Rock):
+                    case (Token::Rock):
                         std::cout << "You chose Rock" << std::endl;
                         break;
-                    case (gameVariable::Paper):
+                    case (Token::Paper):
                         std::cout << "You chose Paper" << std::endl;
                         break;
-                    case (gameVariable::Scissor):
+                    case (Token::Scissor):
                         std::cout << "You chose Scissor" << std::endl;
                         break;
                     default:
@@ -153,7 +132,7 @@ int main()
                         losses++;
                         continue;
                     }
-                    r = calculateResult(key, two, three, four);
+                    r = calculateResult(key);
                     message(r);
                     switch (r) {
                     case (0):
@@ -165,14 +144,6 @@ int main()
                     case (99):
                         draws++;
                     }
-                    four = three;
-                    three = two;
-                    two = key;
-#ifdef _DEBUG
-                    std::cout << " ++++++++R++++++++: " << r << std::endl;
-                    std::cout << "two: " << static_cast<char>(two) << std::endl;
-                    std::cout << "three: " << static_cast<char>(three) << std::endl;
-#endif // DEBUG
                 }
                 std::cout << "wins: " << wins << std::endl;
                 std::cout << "losses: " << losses << std::endl;
